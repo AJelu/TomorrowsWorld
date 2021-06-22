0a0365c748839d6df7a773b82625d2a9007fd781
Jelu <leluk.olexandr@gmail.com>
2021-06-22 16:45:41 +0300

    fix error starting mesh
    about change folder structure

--- Source/TomorrowsWorld/TomorrowsWorldGameMode.cpp
+++ Source/TomorrowsWorld/TomorrowsWorldGameMode.cpp
@@ -7,7 +7,7 @@
 ATomorrowsWorldGameMode::ATomorrowsWorldGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Meshes/TestPlayer/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
