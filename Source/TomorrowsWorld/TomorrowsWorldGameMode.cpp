91dd1c51d179b8c613764391d35f65a1aeca63c3
Jelu <leluk.olexandr@gmail.com>
2021-07-03 11:50:34 +0300

    widgets main & choice levels
    realize menu buttons / settings / options / choice hero images

--- Source/TomorrowsWorld/TomorrowsWorldGameMode.cpp
+++ Source/TomorrowsWorld/TomorrowsWorldGameMode.cpp
@@ -7,7 +7,7 @@
 ATomorrowsWorldGameMode::ATomorrowsWorldGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Meshes/TestPlayer/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Units/TestPlayer/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
